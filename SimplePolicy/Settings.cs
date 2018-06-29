c35d76c9531166f8c599699fdc70a685ad7c4e2a
erikzhang <erik@neo.org>
2018-06-29 17:23:47 +0800

    add SimplePolicy

---
+++ SimplePolicy/Settings.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.Extensions.Configuration;
+using Neo.Wallets;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+
+namespace Neo.Plugins
+{
+    internal class Settings
+    {
+        public int MaxTransactionsPerBlock { get; }
+        public int MaxFreeTransactionsPerBlock { get; }
+        public BlockedAccounts BlockedAccounts { get; }
+
+        public static Settings Default { get; }
+
+        static Settings()
+        {
+            string path = Path.Combine(Assembly.GetExecutingAssembly().GetName().Name, "config.json");
+            IConfigurationSection section = new ConfigurationBuilder().AddJsonFile(path).Build().GetSection("PluginConfiguration");
+            Default = new Settings(section);
+        }
+
+        public Settings(IConfigurationSection section)
+        {
+            this.MaxTransactionsPerBlock = GetValueOrDefault(section.GetSection("MaxTransactionsPerBlock"), 500, p => int.Parse(p));
+            this.MaxFreeTransactionsPerBlock = GetValueOrDefault(section.GetSection("MaxFreeTransactionsPerBlock"), 20, p => int.Parse(p));
+            this.BlockedAccounts = new BlockedAccounts(section.GetSection("BlockedAccounts"));
+        }
+
+        public T GetValueOrDefault<T>(IConfigurationSection section, T defaultValue, Func<string, T> selector)
+        {
+            if (section.Value == null) return defaultValue;
+            return selector(section.Value);
+        }
+    }
+
+    internal enum PolicyType : byte
+    {
+        AllowAll,
+        DenyAll,
+        AllowList,
+        DenyList
+    }
+
+    internal class BlockedAccounts
+    {
+        public PolicyType Type { get; }
+        public HashSet<UInt160> List { get; }
+
+        public BlockedAccounts(IConfigurationSection section)
+        {
+            this.Type = (PolicyType)Enum.Parse(typeof(PolicyType), section.GetSection("Type").Value, true);
+            this.List = new HashSet<UInt160>(section.GetSection("List").GetChildren().Select(p => Wallet.ToScriptHash(p.Value)));
+        }
+    }
+}
---
+++ SimplePolicy/SimplePolicyPlugin.cs
@@ -0,0 +1,48 @@
+﻿using Neo.Network.P2P.Payloads;
+using Neo.SmartContract;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Neo.Plugins
+{
+    public class SimplePolicyPlugin : PolicyPlugin
+    {
+        public override string Name => nameof(SimplePolicyPlugin);
+
+        protected override bool CheckPolicy(Transaction tx)
+        {
+            switch (Settings.Default.BlockedAccounts.Type)
+            {
+                case PolicyType.AllowAll:
+                    return true;
+                case PolicyType.AllowList:
+                    return tx.Witnesses.All(p => Settings.Default.BlockedAccounts.List.Contains(p.VerificationScript.ToScriptHash())) || tx.Outputs.All(p => Settings.Default.BlockedAccounts.List.Contains(p.ScriptHash));
+                case PolicyType.DenyList:
+                    return tx.Witnesses.All(p => !Settings.Default.BlockedAccounts.List.Contains(p.VerificationScript.ToScriptHash())) && tx.Outputs.All(p => !Settings.Default.BlockedAccounts.List.Contains(p.ScriptHash));
+                default:
+                    return false;
+            }
+        }
+
+        protected override IEnumerable<Transaction> Filter(IEnumerable<Transaction> transactions)
+        {
+            Transaction[] array = transactions.ToArray();
+            if (array.Length + 1 <= Settings.Default.MaxTransactionsPerBlock)
+                return array;
+            transactions = array.OrderByDescending(p => p.NetworkFee / p.Size).Take(Settings.Default.MaxTransactionsPerBlock - 1);
+            return FilterFree(transactions);
+        }
+
+        private IEnumerable<Transaction> FilterFree(IEnumerable<Transaction> transactions)
+        {
+            int count = 0;
+            foreach (Transaction tx in transactions)
+                if (tx.NetworkFee > Fixed8.Zero || tx.SystemFee > Fixed8.Zero)
+                    yield return tx;
+                else if (count++ < Settings.Default.MaxFreeTransactionsPerBlock)
+                    yield return tx;
+                else
+                    yield break;
+        }
+    }
+}
