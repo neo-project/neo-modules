1fa17196049f8a95d05c142cdebd8a3b6f420088
Belane <srbelane@gmail.com>
2018-08-25 03:29:31 +0200

    TransactionSizePolicy

---
+++ TransactionSizePolicy/Settings.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Extensions.Configuration;
+using Neo.Wallets;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace Neo.Plugins
+{
+    internal class Settings
+    {
+        public int MaxTransactionSize { get; }
+        public int MaxFreeTransactionSize { get; }
+        public int TransactionExtraSize { get; }
+        public int ExtraSizeChunk { get; }
+        public int ExtraChunkFee { get; }
+
+        public static Settings Default { get; }
+
+        static Settings()
+        {
+            Default = new Settings(Assembly.GetExecutingAssembly().GetConfiguration());
+        }
+
+        public Settings(IConfigurationSection section)
+        {
+            MaxTransactionSize = GetValueOrDefault(section.GetSection("MaxTransactionSize"), 100000, p => int.Parse(p));
+            MaxFreeTransactionSize = GetValueOrDefault(section.GetSection("MaxFreeTransactionSize"), 1000, p => int.Parse(p));
+
+            TransactionExtraSize = GetValueOrDefault(section.GetSection("TransactionExtraSize"), 2500, p => int.Parse(p));
+            ExtraSizeChunk = GetValueOrDefault(section.GetSection("ExtraSizeChunk"), 700, p => int.Parse(p));
+            ExtraChunkFee = GetValueOrDefault(section.GetSection("ExtraChunkFee"), 1, p => int.Parse(p));
+        }
+
+        public T GetValueOrDefault<T>(IConfigurationSection section, T defaultValue, Func<string, T> selector)
+        {
+            if (section.Value == null) return defaultValue;
+            return selector(section.Value);
+        }
+    }
+}
---
+++ TransactionSizePolicy/TransactionSizePolicy.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using Neo.Network.P2P.Payloads;
+
+namespace Neo.Plugins
+{
+    public class TransactionSizePolicyPlugin : Plugin, IPolicyPlugin
+    {
+        public bool CheckPolicy(Transaction tx)
+        {
+            return VerifySizeLimits(tx);
+        }
+
+        public IEnumerable<Transaction> Filter(IEnumerable<Transaction> transactions)
+        {
+            return transactions;
+        }
+
+        private bool VerifySizeLimits(Transaction tx)
+        {
+            // Not Allow free TX bigger than MaxFreeTransactionSize
+            if (tx.NetworkFee.Equals(0) && tx.Size > Settings.Default.MaxFreeTransactionSize) return false;
+
+            // Not Allow TX bigger than MaxTransactionSize
+            if (tx.Size > Settings.Default.MaxTransactionSize) return false;
+
+            // For TX bigger than TransactionExtraSize require proportional fee
+            if (tx.Size > Settings.Default.TransactionExtraSize)
+            {
+                decimal chunks = (tx.Size - Settings.Default.TransactionExtraSize) / Settings.Default.ExtraSizeChunk;
+                decimal fee = chunks * Settings.Default.ExtraChunkFee;
+
+                if (tx.NetworkFee < Fixed8.FromDecimal(fee)) return false;
+            }
+            return true;
+        }
+    }
+}
