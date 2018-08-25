d30e78881110608ff6bf42777a9c32ca1a81258e
Belane <srbelane@gmail.com>
2018-08-26 01:15:36 +0200

    change default values

--- TransactionSizePolicy/Settings.cs
+++ TransactionSizePolicy/Settings.cs
@@ -13,7 +13,7 @@ namespace Neo.Plugins
         public int MaxFreeTransactionSize { get; }
         public int TransactionExtraSize { get; }
         public int ExtraSizeChunk { get; }
-        public int ExtraChunkFee { get; }
+        public decimal ExtraChunkFee { get; }
 
         public static Settings Default { get; }
 
@@ -25,11 +25,11 @@ namespace Neo.Plugins
         public Settings(IConfigurationSection section)
         {
             MaxTransactionSize = GetValueOrDefault(section.GetSection("MaxTransactionSize"), 100000, p => int.Parse(p));
-            MaxFreeTransactionSize = GetValueOrDefault(section.GetSection("MaxFreeTransactionSize"), 1000, p => int.Parse(p));
+            MaxFreeTransactionSize = GetValueOrDefault(section.GetSection("MaxFreeTransactionSize"), 300, p => int.Parse(p));
 
-            TransactionExtraSize = GetValueOrDefault(section.GetSection("TransactionExtraSize"), 2500, p => int.Parse(p));
-            ExtraSizeChunk = GetValueOrDefault(section.GetSection("ExtraSizeChunk"), 700, p => int.Parse(p));
-            ExtraChunkFee = GetValueOrDefault(section.GetSection("ExtraChunkFee"), 1, p => int.Parse(p));
+            TransactionExtraSize = GetValueOrDefault(section.GetSection("TransactionExtraSize"), 2000, p => int.Parse(p));
+            ExtraSizeChunk = GetValueOrDefault(section.GetSection("ExtraSizeChunk"), 100, p => int.Parse(p));
+            ExtraChunkFee = GetValueOrDefault(section.GetSection("ExtraChunkFee"), 0.001M, p => decimal.Parse(p));
         }
 
         public T GetValueOrDefault<T>(IConfigurationSection section, T defaultValue, Func<string, T> selector)
