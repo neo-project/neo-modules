568e96654fd6c079952781446ccd984d3aa17b25
erikzhang <erik@neo.org>
2018-08-29 21:00:32 +0800

    add RpcDisabled

---
+++ RpcDisabled/Settings.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.Configuration;
+using System.Linq;
+using System.Reflection;
+
+namespace Neo.Plugins
+{
+    internal class Settings
+    {
+        public string[] DisabledMethods { get; }
+
+        public static Settings Default { get; }
+
+        static Settings()
+        {
+            Default = new Settings(Assembly.GetExecutingAssembly().GetConfiguration());
+        }
+
+        public Settings(IConfigurationSection section)
+        {
+            this.DisabledMethods = section.GetSection("DisabledMethods").GetChildren().Select(p => p.Value).ToArray();
+        }
+    }
+}
