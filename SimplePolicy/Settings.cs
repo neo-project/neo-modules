c3462c19fc59f1b160174a0c8020358011d98c55
belane <belane@users.noreply.github.com>
2018-11-16 10:32:34 +0100

    Transaction size plugin (#10)

--- SimplePolicy/Settings.cs
+++ SimplePolicy/Settings.cs
@@ -11,6 +11,8 @@ namespace Neo.Plugins
     {
         public int MaxTransactionsPerBlock { get; }
         public int MaxFreeTransactionsPerBlock { get; }
+        public int MaxFreeTransactionSize { get; }
+        public Fixed8 FeePerExtraByte { get; }
         public BlockedAccounts BlockedAccounts { get; }
 
         public static Settings Default { get; }
@@ -24,6 +26,8 @@ namespace Neo.Plugins
         {
             this.MaxTransactionsPerBlock = GetValueOrDefault(section.GetSection("MaxTransactionsPerBlock"), 500, p => int.Parse(p));
             this.MaxFreeTransactionsPerBlock = GetValueOrDefault(section.GetSection("MaxFreeTransactionsPerBlock"), 20, p => int.Parse(p));
+            this.MaxFreeTransactionSize = GetValueOrDefault(section.GetSection("MaxFreeTransactionSize"), 1024, p => int.Parse(p));
+            this.FeePerExtraByte = GetValueOrDefault(section.GetSection("FeePerExtraByte"), Fixed8.FromDecimal(0.00001M), p => Fixed8.Parse(p));
             this.BlockedAccounts = new BlockedAccounts(section.GetSection("BlockedAccounts"));
         }
 
