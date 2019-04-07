b3d49a0990aa0d988f4c70af029c703c18c49a48
jsolman <jsolinsky@gmail.com>
2019-04-07 12:54:09 -0700

    Properly stop importing blocks on system shutdown. (#66)

--- ImportBlocks/ImportBlocks.cs
+++ ImportBlocks/ImportBlocks.cs
@@ -4,47 +4,17 @@ using Neo.Ledger;
 using Neo.Network.P2P.Payloads;
 using Neo.Persistence;
 using System;
-using System.Collections.Generic;
 using System.IO;
-using System.IO.Compression;
-using System.Linq;
-using System.Text.RegularExpressions;
 
 namespace Neo.Plugins
 {
     public class ImportBlocks : Plugin
     {
-        private static bool CheckMaxOnImportHeight(uint currentImportBlockHeight)
-        {
-            if (Settings.Default.MaxOnImportHeight == 0 || Settings.Default.MaxOnImportHeight >= currentImportBlockHeight)
-                return true;
-            return false;
-        }
+        private IActorRef _blockImporter;
 
         public override void Configure()
         {
             Settings.Load(GetConfiguration());
-        }
-
-        private static IEnumerable<Block> GetBlocks(Stream stream, bool read_start = false)
-        {
-            using (BinaryReader r = new BinaryReader(stream))
-            {
-                uint start = read_start ? r.ReadUInt32() : 0;
-                uint count = r.ReadUInt32();
-                uint end = start + count - 1;
-                if (end <= Blockchain.Singleton.Height) yield break;
-                for (uint height = start; height <= end; height++)
-                {
-                    byte[] array = r.ReadBytes(r.ReadInt32());
-                    if (!CheckMaxOnImportHeight(height)) yield break;
-                    if (height > Blockchain.Singleton.Height)
-                    {
-                        Block block = array.AsSerializable<Block>();
-                        yield return block;
-                    }
-                }
-            }
         }
 
         private bool OnExport(string[] args)
@@ -133,50 +103,17 @@ namespace Neo.Plugins
             return true;
         }
 
-        protected override async void OnPluginsLoaded()
+        private void OnImportComplete()
+        {
+            ResumeNodeStartup();
+            System.ActorSystem.Stop(_blockImporter);
+        }
+
+        protected override void OnPluginsLoaded()
         {
             SuspendNodeStartup();
-            const string path_acc = "chain.acc";
-            if (File.Exists(path_acc))
-                using (FileStream fs = new FileStream(path_acc, FileMode.Open, FileAccess.Read, FileShare.Read))
-                    await System.Blockchain.Ask<Blockchain.ImportCompleted>(new Blockchain.Import
-                    {
-                        Blocks = GetBlocks(fs)
-                    });
-            const string path_acc_zip = path_acc + ".zip";
-            if (File.Exists(path_acc_zip))
-                using (FileStream fs = new FileStream(path_acc_zip, FileMode.Open, FileAccess.Read, FileShare.Read))
-                using (ZipArchive zip = new ZipArchive(fs, ZipArchiveMode.Read))
-                using (Stream zs = zip.GetEntry(path_acc).Open())
-                    await System.Blockchain.Ask<Blockchain.ImportCompleted>(new Blockchain.Import
-                    {
-                        Blocks = GetBlocks(zs)
-                    });
-            var paths = Directory.EnumerateFiles(".", "chain.*.acc", SearchOption.TopDirectoryOnly).Concat(Directory.EnumerateFiles(".", "chain.*.acc.zip", SearchOption.TopDirectoryOnly)).Select(p => new
-            {
-                FileName = Path.GetFileName(p),
-                Start = uint.Parse(Regex.Match(p, @"\d+").Value),
-                IsCompressed = p.EndsWith(".zip")
-            }).OrderBy(p => p.Start);
-            foreach (var path in paths)
-            {
-                if (path.Start > Blockchain.Singleton.Height + 1) break;
-                if (path.IsCompressed)
-                    using (FileStream fs = new FileStream(path.FileName, FileMode.Open, FileAccess.Read, FileShare.Read))
-                    using (ZipArchive zip = new ZipArchive(fs, ZipArchiveMode.Read))
-                    using (Stream zs = zip.GetEntry(Path.GetFileNameWithoutExtension(path.FileName)).Open())
-                        await System.Blockchain.Ask<Blockchain.ImportCompleted>(new Blockchain.Import
-                        {
-                            Blocks = GetBlocks(zs, true)
-                        });
-                else
-                    using (FileStream fs = new FileStream(path.FileName, FileMode.Open, FileAccess.Read, FileShare.Read))
-                        await System.Blockchain.Ask<Blockchain.ImportCompleted>(new Blockchain.Import
-                        {
-                            Blocks = GetBlocks(fs, true)
-                        });
-            }
-            ResumeNodeStartup();
+            _blockImporter = System.ActorSystem.ActorOf(BlockImporter.Props());
+            _blockImporter.Tell(new BlockImporter.StartImport { BlockchainActorRef = System.Blockchain, OnComplete = OnImportComplete });
         }
 
         protected override bool OnMessage(object message)
