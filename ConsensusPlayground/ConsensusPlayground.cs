be4f688bbaea1f5851a685a64999fdccb874fc40
Vitor <vncoelho@gmail.com>
2019-04-10 11:01:49 -0300

    Draft for P2PLoosy Plugin

---
+++ ConsensusPlayground/ConsensusPlayground.cs
@@ -0,0 +1,43 @@
+﻿using Neo.Network.P2P.Payloads;
+using Neo.Consensus;
+using Neo.Network.P2P;
+using System;
+
+
+namespace Neo.Plugins
+{
+    public class ConsensusPlayground : Plugin, IP2PPlugin
+    {
+        private static Random RandomGnrt = new Random();
+
+        public override void Configure()
+        {
+            Settings.Load(GetConfiguration());
+        }
+
+        public bool OnP2PMessage(Message message)
+        {
+            return true;
+        }
+
+        public bool OnConsensusMessage(ConsensusPayload payload)
+        {
+            if (payload.ConsensusMessage.Type == ConsensusMessageType.PrepareRequest)
+                return (RandomGnrt.NextDouble() > Settings.Default.ProbRejectPrepRequest);
+
+            if (payload.ConsensusMessage.Type == ConsensusMessageType.PrepareResponse)
+                return (RandomGnrt.NextDouble() > Settings.Default.ProbRejectPrepResponse);
+            
+            if (payload.ConsensusMessage.Type == ConsensusMessageType.Commit)
+                return (RandomGnrt.NextDouble() > Settings.Default.ProbRejectCommit);
+
+            if (payload.ConsensusMessage.Type == ConsensusMessageType.ChangeView)
+                return (RandomGnrt.NextDouble() > Settings.Default.ProbRejectChangeView);
+
+            if (payload.ConsensusMessage.Type == ConsensusMessageType.RecoveryMessage)
+                return (RandomGnrt.NextDouble() > Settings.Default.ProbRejectRecover);
+
+            return true;
+        }   
+    }
+}
---
+++ ConsensusPlayground/Settings.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+
+namespace Neo.Plugins
+{
+    internal class Settings
+    {
+        /// <summary>
+        /// Probability of rejecting consensus messages - by type
+        /// </summary>
+        public double ProbRejectPrepRequest { get; }
+        public double ProbRejectPrepResponse { get; }
+        public double ProbRejectCommit { get; }
+        public double ProbRejectChangeView { get; }
+        public double ProbRejectRecover { get; }
+
+        public static Settings Default { get; private set; }
+
+        private Settings(IConfigurationSection section)
+        {
+            this.ProbRejectPrepRequest = GetValueOrDefault(section.GetSection("ProbRejectPrepRequest"), 0.25, p => double.Parse(p));
+            this.ProbRejectPrepResponse = GetValueOrDefault(section.GetSection("ProbRejectPrepResponse"), 0.5, p => double.Parse(p));
+            this.ProbRejectCommit = GetValueOrDefault(section.GetSection("ProbRejectCommit"), 0.5, p => double.Parse(p));
+            this.ProbRejectChangeView = GetValueOrDefault(section.GetSection("ProbRejectChangeView"), 0, p => double.Parse(p));
+            this.ProbRejectRecover = GetValueOrDefault(section.GetSection("ProbRejectRecover"), 0, p => double.Parse(p));
+        }
+
+        public T GetValueOrDefault<T>(IConfigurationSection section, T defaultValue, Func<string, T> selector)
+        {
+            if (section.Value == null) return defaultValue;
+            return selector(section.Value);
+        }
+
+        public static void Load(IConfigurationSection section)
+        {
+            Default = new Settings(section);
+        }
+    }
+}
