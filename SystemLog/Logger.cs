3e6a6bcebc1169628d6d21891cf938e8cb7fafc3
Shargon <shargon@gmail.com>
2019-08-12 09:33:21 +0200

    Write to file

--- SystemLog/Logger.cs
+++ SystemLog/Logger.cs
@@ -1,5 +1,5 @@
-﻿using Neo.Plugins;
-using System;
+﻿using System;
+using System.IO;
 
 namespace Neo.Plugins
 {
@@ -14,22 +14,30 @@ namespace Neo.Plugins
 
         public void Log(string source, LogLevel level, string message)
         {
-            if (!Settings.Default.ConsoleOutput) return;
+            var log = $"[{DateTime.Now.TimeOfDay:hh\\:mm\\:ss\\.fff}] {message}";
 
-            var currentColor = new ConsoleColorSet();
-
-            switch (level)
+            if (Settings.Default.ConsoleOutput)
             {
-                case LogLevel.Debug: ConsoleColorSet.Debug.Apply(); break;
-                case LogLevel.Error: ConsoleColorSet.Error.Apply(); break;
-                case LogLevel.Fatal: ConsoleColorSet.Fatal.Apply(); break;
-                case LogLevel.Info: ConsoleColorSet.Info.Apply(); break;
-                case LogLevel.Warning: ConsoleColorSet.Warning.Apply(); break;
+                var currentColor = new ConsoleColorSet();
+
+                switch (level)
+                {
+                    case LogLevel.Debug: ConsoleColorSet.Debug.Apply(); break;
+                    case LogLevel.Error: ConsoleColorSet.Error.Apply(); break;
+                    case LogLevel.Fatal: ConsoleColorSet.Fatal.Apply(); break;
+                    case LogLevel.Info: ConsoleColorSet.Info.Apply(); break;
+                    case LogLevel.Warning: ConsoleColorSet.Warning.Apply(); break;
+                }
+
+                Console.WriteLine(log);
+                currentColor.Apply();
             }
 
-            Console.WriteLine($"[{DateTime.Now.TimeOfDay:hh\\:mm\\:ss\\.fff}] {message}");
-
-            currentColor.Apply();
+            if (!string.IsNullOrEmpty(Settings.Default.Path))
+            {
+                var path = Path.Combine(Settings.Default.Path, $"{DateTime.Now:yyyy-MM-dd}.log");
+                File.AppendAllText(path, log + Environment.NewLine);
+            }
         }
     }
 }
--- SystemLog/Settings.cs
+++ SystemLog/Settings.cs
@@ -4,12 +4,14 @@ namespace Neo.Plugins
 {
     internal class Settings
     {
+        public string Path { get; }
         public bool ConsoleOutput { get; }
 
         public static Settings Default { get; private set; }
 
         private Settings(IConfigurationSection section)
         {
+            this.Path = string.Format(section.GetSection("Path").Value, ProtocolSettings.Default.Magic.ToString("X8"));
             this.ConsoleOutput = section.GetSection("ConsoleOutput").Get<bool>();
         }
 
