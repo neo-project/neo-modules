11627cbc4d8c01622ef9752bf4ee307fa4d917b8
Shargon <shargon@gmail.com>
2019-08-12 08:21:50 +0200

    SystemLog plugin

---
+++ SystemLog/Logger.cs
@@ -0,0 +1,35 @@
+﻿using Neo.Plugins;
+using System;
+
+namespace SystemLog
+{
+    public class Logger : Plugin, ILogPlugin
+    {
+        public override string Name => "SystemLogs";
+
+        public override void Configure()
+        {
+            Settings.Load(GetConfiguration());
+        }
+
+        public void Log(string source, LogLevel level, string message)
+        {
+            if (!Settings.Default.ConsoleOutput) return;
+
+            var currentColor = new ConsoleColorSet();
+
+            switch (level)
+            {
+                case LogLevel.Debug: ConsoleColorSet.Debug.Apply(); break;
+                case LogLevel.Error: ConsoleColorSet.Error.Apply(); break;
+                case LogLevel.Fatal: ConsoleColorSet.Fatal.Apply(); break;
+                case LogLevel.Info: ConsoleColorSet.Info.Apply(); break;
+                case LogLevel.Warning: ConsoleColorSet.Warning.Apply(); break;
+            }
+
+            Console.WriteLine($"[{DateTime.Now.TimeOfDay:hh\\:mm\\:ss\\.fff}] {message}");
+
+            currentColor.Apply();
+        }
+    }
+}
---
+++ SystemLog/Settings.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace SystemLog
+{
+    internal class Settings
+    {
+        public bool ConsoleOutput { get; }
+
+        public static Settings Default { get; private set; }
+
+        private Settings(IConfigurationSection section)
+        {
+            this.ConsoleOutput = section.GetSection("ConsoleOutput").Get<bool>();
+        }
+
+        public static void Load(IConfigurationSection section)
+        {
+            Default = new Settings(section);
+        }
+    }
+}
