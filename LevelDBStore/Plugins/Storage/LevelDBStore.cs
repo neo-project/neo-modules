6ce74c34abf613ee188279adad2ebe46465a05f0
erikzhang <erik@neo.org>
2019-12-01 23:22:39 +0800

    Add LevelDBStore

---
+++ LevelDBStore/Plugins/Storage/LevelDBStore.cs
@@ -0,0 +1,19 @@
+﻿using Neo.Persistence;
+
+namespace Neo.Plugins.Storage
+{
+    public class LevelDBStore : Plugin, IStoragePlugin
+    {
+        private string path;
+
+        public override void Configure()
+        {
+            path = string.Format(GetConfiguration().GetSection("Path").Value, ProtocolSettings.Default.Magic.ToString("X8"));
+        }
+
+        public IStore GetStore()
+        {
+            return new Store(path);
+        }
+    }
+}
