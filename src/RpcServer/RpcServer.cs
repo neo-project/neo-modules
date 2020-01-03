2c6ba935770b855d6f583e3129466eaee47ee6b9
Shargon <shargon@gmail.com>
2020-01-03 10:00:24 +0100

    Add some limits to rpc

--- src/RpcServer/RpcServer.cs
+++ src/RpcServer/RpcServer.cs
@@ -91,6 +91,10 @@ namespace Neo.Plugins
         {
             host = new WebHostBuilder().UseKestrel(options => options.Listen(Settings.Default.BindAddress, Settings.Default.Port, listenOptions =>
             {
+                options.Limits.MaxConcurrentConnections = 40;
+                options.Limits.KeepAliveTimeout = TimeSpan.FromMinutes(1);
+                options.Limits.RequestHeadersTimeout = TimeSpan.FromSeconds(15);
+
                 if (string.IsNullOrEmpty(Settings.Default.SslCert)) return;
                 listenOptions.UseHttps(Settings.Default.SslCert, Settings.Default.SslCertPassword, httpsConnectionAdapterOptions =>
                 {
