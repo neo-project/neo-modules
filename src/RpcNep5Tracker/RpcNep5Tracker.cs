0dce97cf7d1c6425027ed893f5a3d8baaf0269b3
bettybao1209 <1062108372@qq.com>
2020-02-26 15:30:58 +0800

    bug ifix

--- src/RpcNep5Tracker/RpcNep5Tracker.cs
+++ src/RpcNep5Tracker/RpcNep5Tracker.cs
@@ -109,16 +109,16 @@ namespace Neo.Plugins
             if (eventName != "Transfer") return;
             if (stateItems.Count < 4) return;
 
-            if (!stateItems[1].IsNull && !(stateItems[1] is VM.Types.ByteArray))
+            if (!(stateItems[1] is null) && !(stateItems[1] is VM.Types.ByteArray))
                 return;
-            if (!stateItems[2].IsNull && !(stateItems[2] is VM.Types.ByteArray))
+            if (!(stateItems[2] is null) && !(stateItems[2] is VM.Types.ByteArray))
                 return;
             var amountItem = stateItems[3];
             if (!(amountItem is VM.Types.ByteArray || amountItem is VM.Types.Integer))
                 return;
-            byte[] fromBytes = stateItems[1].IsNull ? null : stateItems[1].GetSpan().ToArray();
+            byte[] fromBytes = stateItems[1]?.GetSpan().ToArray();
             if (fromBytes?.Length != UInt160.Length) fromBytes = null;
-            byte[] toBytes = stateItems[2].IsNull ? null : stateItems[2].GetSpan().ToArray();
+            byte[] toBytes = stateItems[2]?.GetSpan().ToArray();
             if (toBytes?.Length != UInt160.Length) toBytes = null;
             if (fromBytes == null && toBytes == null) return;
             var from = UInt160.Zero;
