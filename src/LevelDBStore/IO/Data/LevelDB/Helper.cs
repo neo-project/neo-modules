6479bff097b90b33f898916bf05c7f7353c592ff
Luchuan <luchuan@ngd.neo.org>
2020-07-07 22:27:08 +0800

    Update Helper.cs

--- src/LevelDBStore/IO/Data/LevelDB/Helper.cs
+++ src/LevelDBStore/IO/Data/LevelDB/Helper.cs
@@ -44,7 +44,7 @@ namespace Neo.IO.Data.LevelDB
 
         public static byte[] CreateKey(byte table, byte[] key = null)
         {
-            if (key is null) return new[] { table };
+            if (key is null || key.Length == 0) return new[] { table };
             byte[] buffer = new byte[1 + key.Length];
             buffer[0] = table;
             Buffer.BlockCopy(key, 0, buffer, 1, key.Length);
