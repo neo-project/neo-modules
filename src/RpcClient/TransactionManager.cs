d72be970f0a674258cbd0d2462168b8dc49a834c
ProDog <gripzhang@outlook.com>
2020-07-17 17:04:57 +0800

    fix format

--- src/RpcClient/TransactionManager.cs
+++ src/RpcClient/TransactionManager.cs
@@ -66,7 +66,7 @@ namespace Neo.Network.RPC
                 Script = script,
                 Signers = signers,
                 ValidUntilBlock = height + Transaction.MaxValidUntilBlockIncrement,
-                Attributes = attributes ?? Array.Empty<TransactionAttribute>(),                
+                Attributes = attributes ?? Array.Empty<TransactionAttribute>(),
             };
 
             RpcInvokeResult result = rpcClient.InvokeScript(script, signers);
