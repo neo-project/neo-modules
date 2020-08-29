05b3f94d82958f0c2e619e88be7e21c7b1ced279
Harry <harrypierson@ngd.neo.org>
2020-08-28 18:23:37 -0700

    remove assertEx

--- tests/Neo.Network.RPC.Tests/UT_TransactionManager.cs
+++ tests/Neo.Network.RPC.Tests/UT_TransactionManager.cs
@@ -162,7 +162,30 @@ namespace Neo.Network.RPC.Tests
             Assert.AreEqual(1, txManager.Tx.Witnesses.Length);
 
             // throw exception when the KeyPair is wrong
-            await AssertEx.ThrowsAsync<Exception>(async () => await txManager.AddSignature(keyPair2).SignAsync());
+            await ThrowsAsync<Exception>(async () => await txManager.AddSignature(keyPair2).SignAsync());
+        }
+
+        static async Task<TException> ThrowsAsync<TException>(Func<Task> action, bool allowDerivedTypes = true)
+            where TException : Exception
+        {
+            try
+            {
+                await action();
+            }
+            catch (Exception ex)
+            {
+                if (allowDerivedTypes && !(ex is TException))
+                    throw new Exception("Delegate threw exception of type " +
+                    ex.GetType().Name + ", but " + typeof(TException).Name +
+                    " or a derived type was expected.", ex);
+                if (!allowDerivedTypes && ex.GetType() != typeof(TException))
+                    throw new Exception("Delegate threw exception of type " +
+                    ex.GetType().Name + ", but " + typeof(TException).Name +
+                    " was expected.", ex);
+                return (TException)ex;
+            }
+            throw new Exception("Delegate did not throw expected exception " +
+            typeof(TException).Name + ".");
         }
 
         [TestMethod]
