68a2f4d30f00ea698026445f008053a63998324f
Tommo-L <luchuan@ngd.neo.org>
2020-09-01 18:19:55 +0800

    fix

--- src/Oracle/Oracle.cs
+++ src/Oracle/Oracle.cs
@@ -26,46 +26,53 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Timers;
+using static System.IO.Path;
 
 namespace Neo.Plugins
 {
     public class Oracle : Plugin
     {
-        private NEP6Wallet wallet;
-        private string[] nodes;
-        private readonly ConcurrentDictionary<ulong, OracleTask> pendingQueue;
-        private CancellationTokenSource cancelSource;
+        private NEP6Wallet Wallet;
+        private string[] Nodes;
+        private ConcurrentDictionary<ulong, OracleTask> PendingQueue;
+        private HashSet<ulong> ProcessingRequest;
+        private CancellationTokenSource CancelSource;
+        private TimeSpan MaxTaskTimeout;
+        private TimeSpan HttpTimeout;
 
-        private static readonly IOracleProtocol Https = new OracleHttpProtocol();
-        private static readonly TimeSpan MaxTaskTimeout = TimeSpan.FromMinutes(5);
-        public override string Description => "Neo3 built-in oracle plugin";
+        private static readonly OracleHttpProtocol Https = new OracleHttpProtocol();
+
+        public override string Description => "Built-in oracle plugin";
 
         public Oracle()
         {
-            pendingQueue = new ConcurrentDictionary<ulong, OracleTask>();
+            PendingQueue = new ConcurrentDictionary<ulong, OracleTask>();
+            ProcessingRequest = new HashSet<ulong>();
 
-            //RpcServerPlugin.RegisterMethods(this);
+            RpcServerPlugin.RegisterMethods(this);
 
             System.Timers.Timer timer = new System.Timers.Timer();
             timer.Enabled = true;
             timer.Interval = 1000 * 60 * 3;
             timer.Start();
             timer.Elapsed += new ElapsedEventHandler(CleanOutOfDateTask);
-
-            Console.WriteLine("init oracle...");
         }
 
         protected override void Configure()
-        {   
-            nodes = GetConfiguration().GetSection("Nodes").GetChildren().Select(p => p.Get<string>()).ToArray();
-            wallet = new NEP6Wallet(GetConfiguration().GetSection("Wallet").ToString());
+        {
+            var config = GetConfiguration();
+            Nodes = config.GetSection("Nodes").GetChildren().Select(p => p.Get<string>()).ToArray();
+            MaxTaskTimeout = TimeSpan.FromSeconds(uint.Parse(config.GetSection("MaxTaskTimeout").Value));
+            HttpTimeout = TimeSpan.FromSeconds(uint.Parse(config.GetSection("HttpTimeout").Value));
+            Wallet = new NEP6Wallet(Combine(PluginsDirectory, nameof(Oracle), config.GetSection("Wallet").Value));
+            Https.AllowPrivateHost = bool.Parse(config.GetSection("AllowPrivateHost").Value);
         }
 
         [RpcMethod]
         public void SubmitOracleReponseTxSignData(JArray _params)
         {
             var data = _params[0].ToString().HexToBytes();
-            if (data.Length != 105) throw new RpcException(-100, "The length of data should be 105");
+            if (data.Length != 105) throw new RpcException(-100, "The length of data should be  105");
 
             ECPoint oraclePub = ECPoint.Parse(data.Take(33).ToArray().ToHexString(), ECCurve.Secp256r1);
             ulong requestId = BitConverter.ToUInt64(data.Skip(33).Take(8).ToArray());
@@ -79,35 +86,45 @@ namespace Neo.Plugins
         }
 
         [ConsoleCommand("start oracle", Category = "Oracle", Description = "Start oracle service")]
-        private void OnStart(String password)
+        private void OnStart(string password)
         {
-            wallet.Unlock(password);
+            Wallet.Unlock(password);
 
             var snapshot = Blockchain.Singleton.GetSnapshot();
             var oracles = NativeContract.Oracle.GetOracleNodes(snapshot)
                 .Select(u => Contract.CreateSignatureRedeemScript(u).ToScriptHash());
 
-            var accounts = wallet?.GetAccounts()
+            var accounts = Wallet?.GetAccounts()
                 .Where(u => u.HasKey && !u.Lock && oracles.Contains(u.ScriptHash))
                 .Select(u => (u.Contract, u.GetKey()))
                 .ToArray();
+
             if (accounts.Length == 0) throw new ArgumentException("The wallet doesn't have any oracle accounts");
 
-            Interlocked.Exchange(ref cancelSource, new CancellationTokenSource())?.Cancel();
+            Interlocked.Exchange(ref CancelSource, new CancellationTokenSource())?.Cancel();
 
             new Task(() =>
             {
-                while (true)
+                while (CancelSource?.IsCancellationRequested == false)
                 {
-                    if (cancelSource.IsCancellationRequested)
-                        return;
-
                     snapshot = Blockchain.Singleton.GetSnapshot();
                     var enumerator = NativeContract.Oracle.GetRequests(snapshot).GetEnumerator();
-                    while (enumerator.MoveNext() && !cancelSource.IsCancellationRequested)
-                        ProcessRequest(snapshot, enumerator.Current.Item1, enumerator.Current.Item2);
+                    while (enumerator.MoveNext() && !CancelSource.IsCancellationRequested)
+                    {
+                        if (ProcessingRequest.Contains(enumerator.Current.Item1)) continue;
+                        ProcessingRequest.Add(enumerator.Current.Item1);
 
-                    Thread.Sleep(200);
+                        try
+                        {
+                            ProcessRequest(snapshot, enumerator.Current.Item1, enumerator.Current.Item2);
+                        }
+                        catch (Exception e)
+                        {
+                            Log(e, LogLevel.Error);
+                        }
+                    }
+
+                    Thread.Sleep(2000);
                 }
             }).Start();
         }
@@ -115,7 +132,7 @@ namespace Neo.Plugins
         [ConsoleCommand("stop oracle", Category = "Oracle", Description = "Stop oracle service")]
         private void OnStop()
         {
-            Interlocked.Exchange(ref cancelSource, null)?.Cancel();
+            Interlocked.Exchange(ref CancelSource, null)?.Cancel();
         }
 
         public void SendResponseSignature(ulong requestId, byte[] txSign, KeyPair keyPair)
@@ -123,13 +140,13 @@ namespace Neo.Plugins
             var message = keyPair.PublicKey.ToArray().Concat(BitConverter.GetBytes(requestId)).Concat(txSign).ToArray();
             var sign = Crypto.Sign(message, keyPair.PrivateKey, keyPair.PublicKey.ToArray());
 
-            foreach (var node in nodes)
+            foreach (var node in Nodes)
             {
                 new Task(() =>
                 {
                     try
                     {
-                        var url = new Uri("http://" + node + "/");
+                        var url = new Uri(node);
                         HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
                         request.Method = "POST";
                         request.ContentType = "application/json";
@@ -162,7 +179,8 @@ namespace Neo.Plugins
                 case "https":
                     try
                     {
-                        response = new OracleResponse() { Id = requestId, Success = true, Result = Https.Request(requestId, request.Url, request.Filter) };
+                        var data = Https.Request(requestId, request.Url, request.Filter);
+                        response = new OracleResponse() { Id = requestId, Success = true, Result = data };
                     }
                     catch (Exception e)
                     {
@@ -181,7 +199,7 @@ namespace Neo.Plugins
             Log($"Builded response tx:{responseTx.Hash} requestTx:{request.OriginalTxid} requestId: {requestId}");
 
             ECPoint[] oraclePublicKeys = NativeContract.Oracle.GetOracleNodes(snapshot);
-            foreach (var account in wallet.GetAccounts())
+            foreach (var account in Wallet.GetAccounts())
             {
                 var oraclePub = account.GetKey().PublicKey;
                 if (!account.HasKey || account.Lock || !oraclePublicKeys.ToList().Contains(oraclePub)) continue;
@@ -197,7 +215,9 @@ namespace Neo.Plugins
         private Transaction CreateResponseTx(StoreView snapshot, OracleResponse response)
         {
             var oracleNodes = NativeContract.Oracle.GetOracleNodes(snapshot);
-            var oracleScriptHash = Contract.CreateMultiSigContract(oracleNodes.Length - oracleNodes.Length / 3, oracleNodes).ScriptHash;
+            var m = oracleNodes.Length - (oracleNodes.Length - 1) / 3;
+            var n = oracleNodes.Length;
+            var oracleSignContract = Contract.CreateMultiSigContract(m, oracleNodes);
 
             var tx = new Transaction()
             {
@@ -211,42 +231,80 @@ namespace Neo.Plugins
                     new Signer()
                     {
                         Account = NativeContract.Oracle.Hash,
-                        AllowedContracts = new UInt160[]{ },
+                        AllowedContracts = new UInt160[] { },
                         Scopes = WitnessScope.None
                     },
                     new Signer()
                     {
-                        Account = oracleScriptHash,
-                        AllowedContracts = new UInt160[]{ NativeContract.Oracle.Hash },
-                        Scopes = WitnessScope.CalledByEntry
+                        Account = oracleSignContract.ScriptHash,
+                        AllowedContracts = new UInt160[] { NativeContract.Oracle.Hash },
+                        Scopes = WitnessScope.None
                     },
                 },
-                Witnesses = new Witness[0],
+                Witnesses = new Witness[2],
                 Script = OracleResponse.FixedScript,
                 NetworkFee = 0,
                 Nonce = 0,
                 SystemFee = 0
             };
+            Dictionary<UInt160, Witness> witnessDict = new Dictionary<UInt160, Witness>();
+            witnessDict[oracleSignContract.ScriptHash] = new Witness
+            {
+                InvocationScript = Array.Empty<byte>(),
+                VerificationScript = oracleSignContract.Script,
+            };
+            witnessDict[NativeContract.Oracle.Hash] = new Witness
+            {
+                InvocationScript = new ScriptBuilder().EmitPush(0).Emit(OpCode.PACK).EmitPush("verify").ToArray(),
+                VerificationScript = NativeContract.Oracle.Script,
+            };
+
+            UInt160[] hashes = tx.GetScriptHashesForVerifying(snapshot);
+            tx.Witnesses[0] = witnessDict[hashes[0]];
+            tx.Witnesses[1] = witnessDict[hashes[1]];
+
+            // Calcualte system fee
 
             var engine = ApplicationEngine.Run(tx.Script, snapshot, tx);
             if (engine.State != VMState.HALT) return null;
-
             tx.SystemFee = engine.GasConsumed;
-            int size = tx.Size;
-            tx.NetworkFee += wallet.CalculateNetworkFee(snapshot, tx);
+
+
+            // Calculate network fee
+
+            engine = ApplicationEngine.Create(TriggerType.Verification, tx, snapshot.Clone()); 
+            engine.LoadScript(NativeContract.Oracle.Script, CallFlags.None, 0);
+            engine.LoadScript(witnessDict[NativeContract.Oracle.Hash].InvocationScript, CallFlags.None);
+            if (engine.Execute() != VMState.HALT) return null;
+            tx.NetworkFee += engine.GasConsumed;
+
+            // base size for transaction: includes const_header + signers + attributes + script + hashes
+            int size = Transaction.HeaderSize + tx.Signers.GetVarSize() + tx.Attributes.GetVarSize() + tx.Script.GetVarSize() + IO.Helper.GetVarSize(hashes.Length);
+            size += witnessDict[NativeContract.Oracle.Hash].Size;
+            int size_inv = 66 * m;
+            size += IO.Helper.GetVarSize(size_inv) + size_inv + oracleSignContract.Script.GetVarSize();
+            var networkFee = ApplicationEngine.OpCodePrices[OpCode.PUSHDATA1] * m;
+            using (ScriptBuilder sb = new ScriptBuilder())
+                networkFee += ApplicationEngine.OpCodePrices[(OpCode)sb.EmitPush(m).ToArray()[0]];
+            networkFee += ApplicationEngine.OpCodePrices[OpCode.PUSHDATA1] * n;
+            using (ScriptBuilder sb = new ScriptBuilder())
+                networkFee += ApplicationEngine.OpCodePrices[(OpCode)sb.EmitPush(n).ToArray()[0]];
+            networkFee += ApplicationEngine.OpCodePrices[OpCode.PUSHNULL] + ApplicationEngine.ECDsaVerifyPrice * n;
+            tx.NetworkFee += networkFee;
             tx.NetworkFee += size * NativeContract.Policy.GetFeePerByte(snapshot);
             return tx;
         }
 
+
         public void AddResponseTxSign(StoreView snapshot, ulong requestId, byte[] sign, ECPoint oraclePub, Transaction respnoseTx = null)
         {
-            var task = pendingQueue.GetOrAdd(requestId, new OracleTask
+            var task = PendingQueue.GetOrAdd(requestId, new OracleTask
             {
                 Id = requestId,
                 Request = NativeContract.Oracle.GetRequest(snapshot, requestId),
-                Signs = new Dictionary<ECPoint, byte[]>(),
+                Signs = new SortedDictionary<ECPoint, byte[]>(),
             });
-            task.Signs.Add(oraclePub, sign);
+            task.Signs.TryAdd(oraclePub, sign);
             if (respnoseTx != null)
             {
                 task.Tx = respnoseTx;
@@ -264,47 +322,53 @@ namespace Neo.Plugins
             if (task.Signs.Count >= nodes.Length / 3 && task.Tx != null)
             {
                 var contract = Contract.CreateMultiSigContract(nodes.Length - (nodes.Length - 1) / 3, nodes);
-                ContractParametersContext context = new ContractParametersContext(task.Tx);
+                var count = nodes.Length - (nodes.Length - 1) / 3;
+                ScriptBuilder sb = new ScriptBuilder();
                 foreach (var pair in task.Signs)
                 {
-                    context.AddSignature(contract, pair.Key, pair.Value); // TODO 需要验证参数的有效性
-                    if (context.Completed)
-                    {
-                        Log($"Send response tx: responseTx={task.Tx.Hash}");
-
-                        task.Tx.Witnesses = context.GetWitnesses();
-                        pendingQueue.TryRemove(requestId, out _);
-                        System.Blockchain.Tell(new Blockchain.RelayResult { Inventory = task.Tx });
-                        break;
-                    }
+                    sb.EmitPush(pair.Value);
+                    if (--count == 0) break;
                 }
+                var idx = 0;
+                if (task.Tx.GetScriptHashesForVerifying(snapshot)[0] == NativeContract.Oracle.Hash)
+                {
+                    idx = 1;
+                }
+                task.Tx.Witnesses[idx].InvocationScript = sb.ToArray();
+
+                Log($"Send response tx: responseTx={task.Tx.Hash}");
+
+                PendingQueue.TryRemove(requestId, out _);
+                System.Blockchain.Tell(task.Tx);
             }
         }
 
         public void CleanOutOfDateTask(object source, ElapsedEventArgs e)
         {
             List<ulong> outofdates = new List<ulong>();
-            foreach (var outOfDateTask in pendingQueue)
+            foreach (var outOfDateTask in PendingQueue)
             {
                 if (TimeProvider.Current.UtcNow - outOfDateTask.Value.Timestamp <= MaxTaskTimeout)
                     break;
                 outofdates.Add(outOfDateTask.Key);
             }
             foreach (ulong requestId in outofdates)
-                pendingQueue.TryRemove(requestId, out _);
+                PendingQueue.TryRemove(requestId, out _);
+
+            ProcessingRequest.RemoveWhere(requestId => NativeContract.Oracle.GetRequest(Blockchain.Singleton.GetSnapshot(), requestId) is null);
         }
 
         public static void Log(string message, LogLevel level = LogLevel.Info)
         {
             Utility.Log(nameof(Oracle), level, message);
         }
-
+        
         internal class OracleTask
         {
             public ulong Id;
             public OracleRequest Request;
             public Transaction Tx;
-            public Dictionary<ECPoint, byte[]> Signs;
+            public SortedDictionary<ECPoint, byte[]> Signs;
             public readonly DateTime Timestamp = TimeProvider.Current.UtcNow;
         }
     }
--- src/Oracle/Protocol/OracleHttpProtocol.cs
+++ src/Oracle/Protocol/OracleHttpProtocol.cs
@@ -1,7 +1,7 @@
 using System;
 using System.Linq;
+using System.Net;
 using System.Net.Http;
-using System.Text;
 using System.Threading.Tasks;
 using JArray = Newtonsoft.Json.Linq.JArray;
 using JObject = Newtonsoft.Json.Linq.JObject;
@@ -19,6 +19,12 @@ namespace Neo.Plugins
             Utility.Log(nameof(OracleHttpProtocol), LogLevel.Debug, $"Request: {url}");
 
             Uri.TryCreate(url, UriKind.Absolute, out var uri);
+            if (!AllowPrivateHost && IsInternal(Dns.GetHostEntry(uri.Host)))
+            {
+                // Don't allow private host in order to prevent SSRF
+
+                throw new InvalidOperationException("PolicyError");
+            }
             using var handler = new HttpClientHandler
             {
                 // TODO: Accept all certificates
@@ -36,7 +42,7 @@ namespace Neo.Plugins
             var taskRet = result.Result.Content.ReadAsStringAsync();
             if (!taskRet.Wait(Timeout)) throw new InvalidOperationException("Timeout");
             var data = Filter(taskRet.Result, filter);
-            return Encoding.UTF8.GetBytes(data);
+            return Utility.StrictUTF8.GetBytes(data);
         }
 
         private string Filter(string input, string filterArgs)
@@ -47,6 +53,44 @@ namespace Neo.Plugins
             JObject beforeObject = JObject.Parse(input);
             JArray afterObjects = new JArray(beforeObject.SelectTokens(filterArgs).ToArray()); // TODO 
             return afterObjects.ToString();
+        }
+
+        internal static bool IsInternal(IPHostEntry entry)
+        {
+            foreach (var ip in entry.AddressList)
+            {
+                if (IsInternal(ip)) return true;
+            }
+
+            return false;
+        }
+
+        /// <summary>
+        ///       ::1          -   IPv6  loopback
+        ///       10.0.0.0     -   10.255.255.255  (10/8 prefix)
+        ///       127.0.0.0    -   127.255.255.255  (127/8 prefix)
+        ///       172.16.0.0   -   172.31.255.255  (172.16/12 prefix)
+        ///       192.168.0.0  -   192.168.255.255 (192.168/16 prefix)
+        /// </summary>
+        /// <param name="ipAddress">Address</param>
+        /// <returns>True if it was an internal address</returns>
+        internal static bool IsInternal(IPAddress ipAddress)
+        {
+            if (IPAddress.IsLoopback(ipAddress)) return true;
+            if (IPAddress.Broadcast.Equals(ipAddress)) return true;
+            if (IPAddress.Any.Equals(ipAddress)) return true;
+            if (IPAddress.IPv6Any.Equals(ipAddress)) return true;
+            if (IPAddress.IPv6Loopback.Equals(ipAddress)) return true;
+
+            var ip = ipAddress.GetAddressBytes();
+            switch (ip[0])
+            {
+                case 10:
+                case 127: return true;
+                case 172: return ip[1] >= 16 && ip[1] < 32;
+                case 192: return ip[1] == 168;
+                default: return false;
+            }
         }
     }
 }
