6069db452661f4c1c02d6e04fe32047662db4e74
Luchuan <luchuan@ngd.neo.org>
2020-10-15 11:06:10 +0800

    Allow rpc method can be overrided (#367)
    * allow rpc method can be overrided
    
    * change to protected virtual
    
    * set DummyWallet private
    
    * Update src/RpcServer/RpcServer.SmartContract.cs
    
    Co-authored-by: Shargon <shargon@gmail.com>
    
    Co-authored-by: Shargon <shargon@gmail.com>
    Co-authored-by: Owen Zhang <38493437+superboyiii@users.noreply.github.com>

--- src/RpcServer/RpcServer.Blockchain.cs
+++ src/RpcServer/RpcServer.Blockchain.cs
@@ -6,9 +6,7 @@ using Neo.IO.Json;
 using Neo.Ledger;
 using Neo.Network.P2P.Payloads;
 using Neo.Persistence;
-using Neo.SmartContract;
 using Neo.SmartContract.Native;
-using Neo.VM;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -18,13 +16,13 @@ namespace Neo.Plugins
     partial class RpcServer
     {
         [RpcMethod]
-        private JObject GetBestBlockHash(JArray _params)
+        protected virtual JObject GetBestBlockHash(JArray _params)
         {
             return Blockchain.Singleton.CurrentBlockHash.ToString();
         }
 
         [RpcMethod]
-        private JObject GetBlock(JArray _params)
+        protected virtual JObject GetBlock(JArray _params)
         {
             JObject key = _params[0];
             bool verbose = _params.Count >= 2 && _params[1].AsBoolean();
@@ -54,13 +52,13 @@ namespace Neo.Plugins
         }
 
         [RpcMethod]
-        private JObject GetBlockCount(JArray _params)
+        protected virtual JObject GetBlockCount(JArray _params)
         {
             return Blockchain.Singleton.Height + 1;
         }
 
         [RpcMethod]
-        private JObject GetBlockHash(JArray _params)
+        protected virtual JObject GetBlockHash(JArray _params)
         {
             uint height = uint.Parse(_params[0].AsString());
             if (height <= Blockchain.Singleton.Height)
@@ -71,7 +69,7 @@ namespace Neo.Plugins
         }
 
         [RpcMethod]
-        private JObject GetBlockHeader(JArray _params)
+        protected virtual JObject GetBlockHeader(JArray _params)
         {
             JObject key = _params[0];
             bool verbose = _params.Count >= 2 && _params[1].AsBoolean();
@@ -103,7 +101,7 @@ namespace Neo.Plugins
         }
 
         [RpcMethod]
-        private JObject GetContractState(JArray _params)
+        protected virtual JObject GetContractState(JArray _params)
         {
             UInt160 script_hash = UInt160.Parse(_params[0].AsString());
             ContractState contract = Blockchain.Singleton.View.Contracts.TryGet(script_hash);
@@ -111,7 +109,7 @@ namespace Neo.Plugins
         }
 
         [RpcMethod]
-        private JObject GetRawMemPool(JArray _params)
+        protected virtual JObject GetRawMemPool(JArray _params)
         {
             bool shouldGetUnverified = _params.Count >= 1 && _params[0].AsBoolean();
             if (!shouldGetUnverified)
@@ -128,7 +126,7 @@ namespace Neo.Plugins
         }
 
         [RpcMethod]
-        private JObject GetRawTransaction(JArray _params)
+        protected virtual JObject GetRawTransaction(JArray _params)
         {
             UInt256 hash = UInt256.Parse(_params[0].AsString());
             bool verbose = _params.Count >= 2 && _params[1].AsBoolean();
@@ -153,7 +151,7 @@ namespace Neo.Plugins
         }
 
         [RpcMethod]
-        private JObject GetStorage(JArray _params)
+        protected virtual JObject GetStorage(JArray _params)
         {
             if (!int.TryParse(_params[0].AsString(), out int id))
             {
@@ -172,7 +170,7 @@ namespace Neo.Plugins
         }
 
         [RpcMethod]
-        private JObject GetTransactionHeight(JArray _params)
+        protected virtual JObject GetTransactionHeight(JArray _params)
         {
             UInt256 hash = UInt256.Parse(_params[0].AsString());
             uint? height = Blockchain.Singleton.View.Transactions.TryGet(hash)?.BlockIndex;
@@ -181,7 +179,7 @@ namespace Neo.Plugins
         }
 
         [RpcMethod]
-        private JObject GetNextBlockValidators(JArray _params)
+        protected virtual JObject GetNextBlockValidators(JArray _params)
         {
             using SnapshotView snapshot = Blockchain.Singleton.GetSnapshot();
             var validators = NativeContract.NEO.GetNextBlockValidators(snapshot);
@@ -196,7 +194,7 @@ namespace Neo.Plugins
         }
 
         [RpcMethod]
-        private JObject GetCommittee(JArray _params)
+        protected virtual JObject GetCommittee(JArray _params)
         {
             using SnapshotView snapshot = Blockchain.Singleton.GetSnapshot();
             return new JArray(NativeContract.NEO.GetCommittee(snapshot).Select(p => (JObject)p.ToString()));
--- src/RpcServer/RpcServer.SmartContract.cs
+++ src/RpcServer/RpcServer.SmartContract.cs
@@ -98,7 +98,7 @@ namespace Neo.Plugins
         }
 
         [RpcMethod]
-        private JObject InvokeFunction(JArray _params)
+        protected virtual JObject InvokeFunction(JArray _params)
         {
             UInt160 script_hash = UInt160.Parse(_params[0].AsString());
             string operation = _params[1].AsString();
@@ -114,7 +114,7 @@ namespace Neo.Plugins
         }
 
         [RpcMethod]
-        private JObject InvokeScript(JArray _params)
+        protected virtual JObject InvokeScript(JArray _params)
         {
             byte[] script = _params[0].AsString().HexToBytes();
             UInt160 sender = _params.Count >= 2 ? AddressToScriptHash(_params[1].AsString()) : null;
@@ -123,7 +123,7 @@ namespace Neo.Plugins
         }
 
         [RpcMethod]
-        private JObject GetUnclaimedGas(JArray _params)
+        protected virtual JObject GetUnclaimedGas(JArray _params)
         {
             string address = _params[0].AsString();
             JObject json = new JObject();
