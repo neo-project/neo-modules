919df58218f46031e373e0e16eb1b220785cc1b2
Erik Zhang <erik@neo.org>
2020-12-18 20:25:30 +0800

    Move IsInternal to Helper.cs

--- src/OracleService/Protocols/OracleHttpsProtocol.cs
+++ src/OracleService/Protocols/OracleHttpsProtocol.cs
@@ -16,7 +16,7 @@ namespace Neo.Plugins
             NUtility.Log(nameof(OracleHttpsProtocol), LogLevel.Debug, $"Request: {uri.AbsoluteUri}");
 
             response = null;
-            if (!Settings.Default.AllowPrivateHost && IsInternal(Dns.GetHostEntry(uri.Host))) return OracleResponseCode.Forbidden;
+            if (!Settings.Default.AllowPrivateHost && Dns.GetHostEntry(uri.Host).IsInternal()) return OracleResponseCode.Forbidden;
 
             using var handler = new HttpClientHandler();
             using var client = new HttpClient(handler);
@@ -34,43 +34,6 @@ namespace Neo.Plugins
             if (Settings.Default.Https.Timeout <= sw.ElapsedMilliseconds || !taskRet.Wait(Settings.Default.Https.Timeout - (int)sw.ElapsedMilliseconds)) return OracleResponseCode.Timeout;
             response = taskRet.Result;
             return OracleResponseCode.Success;
-        }
-
-        internal static bool IsInternal(IPHostEntry entry)
-        {
-            foreach (var ip in entry.AddressList)
-                if (IsInternal(ip))
-                    return true;
-
-            return false;
-        }
-
-        /// <summary>
-        ///       ::1          -   IPv6  loopback
-        ///       10.0.0.0     -   10.255.255.255  (10/8 prefix)
-        ///       127.0.0.0    -   127.255.255.255  (127/8 prefix)
-        ///       172.16.0.0   -   172.31.255.255  (172.16/12 prefix)
-        ///       192.168.0.0  -   192.168.255.255 (192.168/16 prefix)
-        /// </summary>
-        /// <param name="ipAddress">Address</param>
-        /// <returns>True if it was an internal address</returns>
-        internal static bool IsInternal(IPAddress ipAddress)
-        {
-            if (IPAddress.IsLoopback(ipAddress)) return true;
-            if (IPAddress.Broadcast.Equals(ipAddress)) return true;
-            if (IPAddress.Any.Equals(ipAddress)) return true;
-            if (IPAddress.IPv6Any.Equals(ipAddress)) return true;
-            if (IPAddress.IPv6Loopback.Equals(ipAddress)) return true;
-
-            var ip = ipAddress.GetAddressBytes();
-            switch (ip[0])
-            {
-                case 10:
-                case 127: return true;
-                case 172: return ip[1] >= 16 && ip[1] < 32;
-                case 192: return ip[1] == 168;
-                default: return false;
-            }
         }
     }
 }
