f63a81ab7b0a6aaf57736950c2ea9729c577af8b
Erik Zhang <erik@neo.org>
2020-12-22 15:37:27 +0800

    fix

--- src/OracleService/OracleService.cs
+++ src/OracleService/OracleService.cs
@@ -91,7 +91,6 @@ namespace Neo.Plugins
                 Console.WriteLine("Cancelled");
                 return;
             }
-            started = true;
 
             wallet = new NEP6Wallet(Settings.Default.Wallet);
             try
@@ -109,6 +108,8 @@ namespace Neo.Plugins
                 if (!CheckOracleAvaiblable(snapshot, out ECPoint[] oracles)) throw new ArgumentException("The oracle service is unavailable");
                 if (!CheckOracleAccount(oracles)) throw new ArgumentException("There is no oracle account in wallet");
             }
+
+            started = true;
 
             timer ??= new Timer(OnTimer, null, RefreshInterval, Timeout.Infinite);
 
