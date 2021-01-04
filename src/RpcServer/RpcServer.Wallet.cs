dd4216caa22391606b85a1d8ef9e96ad5d5dc623
陈志同 <bitcoin2077@outlook.com>
2021-01-03 21:12:32 -0600

    Update src/RpcServer/RpcServer.Wallet.cs
    Co-authored-by: cloud8little <34291844+cloud8little@users.noreply.github.com>

--- src/RpcServer/RpcServer.Wallet.cs
+++ src/RpcServer/RpcServer.Wallet.cs
@@ -355,7 +355,7 @@ namespace Neo.Plugins
             tx.Witnesses = context.Completed ? context.GetWitnesses() : null;
 
             using ApplicationEngine engine = ApplicationEngine.Create(TriggerType.Verification, tx, snapshot.Clone());
-            engine.LoadContract(contract, methodName, CallFlags.None, true, args.length);
+            engine.LoadContract(contract, methodName, CallFlags.None, true, (ushort)args.Length);
 
             engine.LoadScript(new ScriptBuilder().EmitDynamicCall(scriptHash, methodName, true, args).ToArray(), (ushort)args.Length, 1);
 
