82e15719556aadd65d982350b17e62d93a753690
Erik Zhang <erik@neo.org>
2021-01-13 17:07:43 +0800

    Add start command

--- src/dBFT/DBFTPlugin.cs
+++ src/dBFT/DBFTPlugin.cs
@@ -1,4 +1,5 @@
 using Akka.Actor;
+using Neo.ConsoleService;
 using Neo.Network.P2P;
 using Neo.Plugins;
 using Neo.Wallets;
@@ -8,14 +9,23 @@ namespace Neo.Consensus
     public class DBFTPlugin : Plugin, IConsensusProvider, IP2PPlugin
     {
         private IActorRef consensus;
+        private bool started = false;
 
         protected override void Configure()
         {
             Settings.Load(GetConfiguration());
         }
 
-        void IConsensusProvider.Start(Wallet wallet)
+        [ConsoleCommand("start consensus", Category = "Consensus", Description = "Start consensus service (dBFT)")]
+        private void OnStart()
         {
+            Start(GetService<IWalletProvider>().GetWallet());
+        }
+
+        public void Start(Wallet wallet)
+        {
+            if (started) return;
+            started = true;
             consensus = System.ActorSystem.ActorOf(ConsensusService.Props(System.LocalNode, System.TaskManager, System.Blockchain, System.LoadStore(Settings.Default.RecoveryLogs), wallet));
             consensus.Tell(new ConsensusService.Start());
         }
