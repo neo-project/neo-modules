39e81a9dd570bb4c9a0b956ced3d2007db0ffed8
ZhangTao1596 <zhangtao@ngd.neo.org>
2021-01-13 17:47:53 +0800

    clean settings

--- src/StateService/Settings.cs
+++ src/StateService/Settings.cs
@@ -8,17 +8,12 @@ namespace Neo.Plugins.StateService
     {
         public string Path { get; }
         public bool FullState { get; }
-        public bool StartValidate { get; }
-        public string[] Validators { get; }
-
         public static Settings Default { get; private set; }
 
         private Settings(IConfigurationSection section)
         {
             Path = string.Format(section.GetSection("Path").Value ?? "Data_MPT_{0}", ProtocolSettings.Default.Magic.ToString("X8"));
             FullState = GetValueOrDefault(section.GetSection("FullState"), false, p => bool.Parse(p));
-            StartValidate = GetValueOrDefault(section.GetSection("StartValidate"), false, p => bool.Parse(p));
-            Validators = section.GetSection("Validators").GetChildren().Select(p => p.Get<string>()).ToArray();
         }
 
         private T GetValueOrDefault<T>(IConfigurationSection section, T defaultValue, Func<string, T> selector)
