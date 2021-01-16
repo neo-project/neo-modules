af163ecf68eda7a6c84fed34708158201f0d354c
Shargon <shargon@gmail.com>
2021-01-16 12:40:15 +0100

    Sync neo

--- src/RpcClient/Models/RpcContractState.cs
+++ src/RpcClient/Models/RpcContractState.cs
@@ -1,32 +1,31 @@
-using Neo;
-using Neo.IO;
 using Neo.IO.Json;
-using Neo.Network.RPC.Models;
 using Neo.SmartContract;
 using Neo.SmartContract.Manifest;
-using System;
 
-public class RpcContractState
+namespace Neo.Network.RPC.Models
 {
-    public ContractState ContractState { get; set; }
-
-    public JObject ToJson()
+    public class RpcContractState
     {
-        return ContractState.ToJson();
-    }
+        public ContractState ContractState { get; set; }
 
-    public static RpcContractState FromJson(JObject json)
-    {
-        return new RpcContractState
+        public JObject ToJson()
         {
-            ContractState = new ContractState
+            return ContractState.ToJson();
+        }
+
+        public static RpcContractState FromJson(JObject json)
+        {
+            return new RpcContractState
             {
-                Id = (int)json["id"].AsNumber(),
-                UpdateCounter = (ushort)json["updatecounter"].AsNumber(),
-                Hash = UInt160.Parse(json["hash"].AsString()),
-                Nef = RpcNefFile.FromJson(json["nef"]),
-                Manifest = ContractManifest.FromJson(json["manifest"])
-            }
-        };
+                ContractState = new ContractState
+                {
+                    Id = (int)json["id"].AsNumber(),
+                    UpdateCounter = (ushort)json["updatecounter"].AsNumber(),
+                    Hash = UInt160.Parse(json["hash"].AsString()),
+                    Nef = RpcNefFile.FromJson(json["nef"]),
+                    Manifest = ContractManifest.FromJson(json["manifest"])
+                }
+            };
+        }
     }
 }
--- src/RpcClient/Models/RpcMethodToken.cs
+++ src/RpcClient/Models/RpcMethodToken.cs
@@ -1,10 +1,6 @@
 using Neo.IO.Json;
 using Neo.SmartContract;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Neo.Network.RPC.Models
 {
