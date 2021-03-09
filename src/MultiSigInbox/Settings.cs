9d4cda11ce8d0a2420f763303a561d514e16a4a2
Erik Zhang <erik@neo.org>
2021-03-09 13:49:38 +0800

    Update Settings.cs

--- src/MultiSigInbox/Settings.cs
+++ src/MultiSigInbox/Settings.cs
@@ -5,13 +5,13 @@ namespace Neo.Plugins.MultiSigInbox
     internal class Settings
     {
         public string Path { get; }
+        public bool AutoStart { get; }
 
         public static Settings Default { get; private set; }
-        public bool AutoStart { get; internal set; }
 
         private Settings(IConfigurationSection section)
         {
-            Path = string.Format(section.GetValue("Path", "Data_MultiSigInbox_{0}"), ProtocolSettings.Default.Magic.ToString("X8"));
+            Path = section.GetValue("Path", "Data_MultiSigInbox_{0}");
             AutoStart = section.GetValue("AutoStart", false);
         }
 
