b1c745be9651dd0f64bd4564a94fcff19658e0f8
ZhangTao1596 <zhangtao@ngd.neo.org>
2021-04-16 17:32:56 +0800

    update morph

--- src/FileStorage/Morph/Invoker/MorphContractInvoker.NetMap.cs
+++ src/FileStorage/Morph/Invoker/MorphContractInvoker.NetMap.cs
@@ -1,7 +1,10 @@
+using Neo.FileStorage.API.Netmap;
 using Neo.VM.Types;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using Array = Neo.VM.Types.Array;
+using static Neo.FileStorage.API.Netmap.Helper;
 
 namespace Neo.FileStorage.Morph.Invoker
 {
@@ -18,12 +21,6 @@ namespace Neo.FileStorage.Morph.Invoker
         private const string EpochSnapshotMethod = "snapshotByEpoch";
         private const long ExtraFee = 0;
 
-        public class UpdateStateArgs
-        {
-            public byte[] key;
-            public long state;
-        }
-
         public static bool InvokeAddPeer(Client client, byte[] info)
         {
             return client.Invoke(out _, NetMapContractHash, AddPeerMethod, ExtraFee, info);
@@ -35,11 +32,11 @@ namespace Neo.FileStorage.Morph.Invoker
             if (result.State != VM.VMState.HALT) throw new Exception("could not invoke method (Config)");
             return result.ResultStack[0].GetSpan().ToArray();
         }
-        public static long InvokeEpoch(Client client)
+        public static ulong InvokeEpoch(Client client)
         {
             InvokeResult result = client.TestInvoke(NetMapContractHash, EpochMethod);
             if (result.State != VM.VMState.HALT) throw new Exception("could not invoke method (Epoch)");
-            return (long)result.ResultStack[0].GetInteger();
+            return (ulong)result.ResultStack[0].GetInteger();
         }
 
         public static bool InvokeNewEpoch(Client client, long epochNumber)
@@ -47,19 +44,19 @@ namespace Neo.FileStorage.Morph.Invoker
             return client.Invoke(out _, NetMapContractHash, NewEpochMethod, ExtraFee, epochNumber);
         }
 
-        public static bool InvokeUpdateState(Client client, UpdateStateArgs args)
+        public static bool InvokeUpdateState(Client client, long state, byte[] key)
         {
-            return client.Invoke(out _, NetMapContractHash, UpdateStateMethod, ExtraFee, args.state, args.key);
+            return client.Invoke(out _, NetMapContractHash, UpdateStateMethod, ExtraFee, state, key);
         }
 
-        public static byte[][] InvokeNetMap(Client client)
+        public static NetMap InvokeNetMap(Client client)
         {
             InvokeResult result = client.TestInvoke(NetMapContractHash, NetMapMethod);
             if (result.State != VM.VMState.HALT) throw new Exception("could not invoke method (NetMap)");
             if (result.ResultStack.Length != 1) throw new Exception(string.Format("unexpected stack item count ({0})", result.ResultStack.Length));
             Array peers = (Array)result.ResultStack[0];
             IEnumerator<StackItem> peersEnumerator = peers.GetEnumerator();
-            List<byte[]> res = new List<byte[]>();
+            List<byte[]> res = new();
             while (peersEnumerator.MoveNext())
             {
                 Array peer = (Array)peersEnumerator.Current;
@@ -70,17 +67,18 @@ namespace Neo.FileStorage.Morph.Invoker
                     res.Add(peerEnumerator.Current.GetSpan().ToArray());
                 }
             }
-            return res.ToArray();
+
+            return new(res.Select(p => NodeInfo.Parser.ParseFrom(p)).ToList().InfoToNodes());
         }
 
-        public static byte[][] InvokeSnapshot(Client client, int different)
+        public static NetMap InvokeSnapshot(Client client, int different)
         {
             InvokeResult result = client.TestInvoke(NetMapContractHash, SnapshotMethod, different);
             if (result.State != VM.VMState.HALT) throw new Exception("could not invoke method (Snapshot)");
             if (result.ResultStack.Length != 1) throw new Exception(string.Format("unexpected stack item count ({0})", result.ResultStack.Length));
             Array peers = (Array)result.ResultStack[0];
             IEnumerator<StackItem> peersEnumerator = peers.GetEnumerator();
-            List<byte[]> res = new List<byte[]>();
+            List<byte[]> res = new();
             while (peersEnumerator.MoveNext())
             {
                 Array peer = (Array)peersEnumerator.Current;
@@ -91,17 +89,17 @@ namespace Neo.FileStorage.Morph.Invoker
                     res.Add(peerEnumerator.Current.GetSpan().ToArray());
                 }
             }
-            return res.ToArray();
+            return new(res.Select(p => NodeInfo.Parser.ParseFrom(p)).ToList().InfoToNodes());
         }
 
-        public static byte[][] InvokeEpochSnapshot(Client client, long epoch)
+        public static NetMap InvokeEpochSnapshot(Client client, long epoch)
         {
             InvokeResult result = client.TestInvoke(NetMapContractHash, EpochSnapshotMethod, epoch);
             if (result.State != VM.VMState.HALT) throw new Exception("could not invoke method (EpochSnapshot)");
             if (result.ResultStack.Length != 1) throw new Exception(string.Format("unexpected stack item count ({0})", result.ResultStack.Length));
             Array peers = (Array)result.ResultStack[0];
             IEnumerator<StackItem> peersEnumerator = peers.GetEnumerator();
-            List<byte[]> res = new List<byte[]>();
+            List<byte[]> res = new();
             while (peersEnumerator.MoveNext())
             {
                 Array peer = (Array)peersEnumerator.Current;
@@ -112,7 +110,7 @@ namespace Neo.FileStorage.Morph.Invoker
                     res.Add(peerEnumerator.Current.GetSpan().ToArray());
                 }
             }
-            return res.ToArray();
+            return new(res.Select(p => NodeInfo.Parser.ParseFrom(p)).ToList().InfoToNodes());
         }
     }
 }
