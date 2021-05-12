0bc36c13e038e2b0adc35e5b3d30da920cd98293
Alex Makarov <alex@cron-foundation.org>
2021-05-12 12:29:10 +0800

    add export assets in SyncBlocks plugin

--- SyncBlocks/ExportService.cs
+++ SyncBlocks/ExportService.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Linq;
 using Cron.Plugins.SyncBlocks.Extensions;
 using Cron.IO;
+using Cron.IO.Json;
 using Cron.Ledger;
 using Cron.Network.P2P.Payloads;
 
@@ -13,26 +14,39 @@ namespace Cron.Plugins.SyncBlocks
     {
         public static void ExportData()
         {
-            Console.WriteLine($"-Start collect blocks: {DateTime.Now}");
+            Console.WriteLine($"-Start export: {DateTime.Now}");
+            Console.WriteLine($"--Start export assets: {DateTime.Now}");
+            var assetsCache = Blockchain.Singleton.Store.GetAssets().Find();
+            var assetFileName = "assets.acc";
+            Console.WriteLine($"+--- Start export assets in file {assetFileName}: {DateTime.Now}");
+            var assets = assetsCache.Select(x => x.Value)
+                .OrderBy(x => x.AssetType)
+                .ThenBy(x => x.Expiration);
+            SerializeAssets(assetFileName, assets);
+            Console.WriteLine($"+--- Finish export assets: {DateTime.Now}");
+            Console.WriteLine($"--Finish export assets: {DateTime.Now}");
+
+            Console.WriteLine($"--Start collect blocks: {DateTime.Now}");
             var dataCache = Blockchain.Singleton.Store.GetBlocks().Find();
             var blockChunks = dataCache.Where(x => x.Value.TrimmedBlock.IsBlock)
                 .Select(x => x.Value.TrimmedBlock.GetBlock(Blockchain.Singleton.Store.GetTransactions()))
                 .OrderBy(x => x.Header.Index)
                 .ChunkBy(1000000);
             
-            Console.WriteLine($"-Finish collect blocks: length = {blockChunks.Sum(x => x.Count)}: {DateTime.Now}");
-            Console.WriteLine($"-Start export blocks: {DateTime.Now}");
+            Console.WriteLine($"--Finish collect blocks: length = {blockChunks.Sum(x => x.Count)}: {DateTime.Now}");
+            Console.WriteLine($"--Start export blocks: {DateTime.Now}");
             var chunkIndex = 1;
             foreach (var blocks in blockChunks)
             {
                 var fileName = $"block_chunk_{chunkIndex}.acc";
-                Console.WriteLine($"+-- Start chunk {chunkIndex} in file {fileName}: {DateTime.Now}");
+                Console.WriteLine($"+--- Start chunk {chunkIndex} in file {fileName}: {DateTime.Now}");
                 BinarySerialize(fileName, blocks);
-                Console.WriteLine($"+-- Finish chunk {chunkIndex} in file {fileName}: {DateTime.Now}");
+                Console.WriteLine($"+--- Finish chunk {chunkIndex} in file {fileName}: {DateTime.Now}");
                 chunkIndex++;
             }
             
-            Console.WriteLine($"-Finish export blocks in file: {DateTime.Now}");
+            Console.WriteLine($"--Finish export blocks in file: {DateTime.Now}");
+            Console.WriteLine($"-Finish export: {DateTime.Now}");
         }
         
         private static void BinarySerialize(string fileName, ICollection<Block> blocks)
@@ -50,6 +64,31 @@ namespace Cron.Plugins.SyncBlocks
                     fs.Write(array, 0, array.Length);
                 }
             }
+        }
+
+        private static void SerializeAssets(string fileName, IEnumerable<AssetState> assets)
+        {
+            var array = new JArray(assets.Select(asset =>
+            {
+                var state = new JObject
+                {
+                    ["id"] = asset.AssetId.ToString(),
+                    ["type"] = asset.AssetType,
+                    ["name"] = asset.GetName(),
+                    ["amount"] = asset.Amount.ToString(),
+                    ["available"] = asset.Available.ToString(),
+                    ["issuer"] = asset.Issuer.ToString(),
+                    ["precision"] = asset.Precision.ToString(),
+                    ["fee"] = asset.Fee.ToString(),
+                    ["feeaddress"] = asset.FeeAddress.ToString(),
+                    ["owner"] = asset.Owner.ToString(),
+                    ["admin"] = asset.Admin.ToString(),
+                    ["expiration"] = asset.Expiration.ToString(),
+                    ["isfrozen"] = asset.IsFrozen
+                };
+                return state;
+            }));
+            File.WriteAllText(fileName, array.ToString());
         }
     }
 }
