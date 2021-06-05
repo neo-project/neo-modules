1884d71ccf2c3825f43c9b5356689af4733db2aa
Jinghui Liao <jinghui@wayne.edu>
2021-06-05 09:06:53 +0800

    fix the prove kbytes length error

--- src/DBFTPlugin/VRF.cs
+++ src/DBFTPlugin/VRF.cs
@@ -215,7 +215,7 @@ namespace Neo.Consensus
 
             // Step 5: k = ECVRF_nonce_generation(SK, h_string)
             var k = GenerateNonce(prikey, h_string);
-            var kBytes = k.ToByteArray(true, true);
+            var kBytes  = AppendLeadingZeros(k.ToByteArray(true, true), qlen);
 
             // Step 6: c = ECVRF_hash_points(H, Gamma, k*B, k*H)
             var u_point = DerivePubkeyPoint(k.ToByteArray(true, true));
