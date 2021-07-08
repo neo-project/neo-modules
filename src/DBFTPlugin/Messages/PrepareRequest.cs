43ae26589f7cece3e0bf2b1feea8198d9bbf69d7
Jinghui Liao <jinghui@wayne.edu>
2021-07-08 11:34:06 +0800

    Add Random number to N3 (#596)
    * Add VRF nonce to the consensus.
    
    * remove unused code
    
    * Add vrf proof to MakeRecoveryMessage and Serialize
    
    * add comment
    
    * Update the feed of VRF
    
    * Clean enter
    
    * Prevent the same primary using the same prevHash in Block 1000..2000
    
    * update comment error
    
    * throw exception when VRF verification fails
    
    * Typo correction
    
    * Update src/DBFTPlugin/PrepareRequest.cs
    
    Co-authored-by: Shargon <shargon@gmail.com>
    
    * Simplify the code
    
    * use prevHash directly
    
    * move VRF from neo core to plugin
    
    * convert Uint256 to Array
    
    * Move `nonce` to a transaction
    
    * Broadcat the nonce_tx and verify nonce
    
    * remove nonce from size
    
    * insert nonce_tx at the begining of txs
    
    * Update the nonce_tx `sender`
    
    * fix length error
    
    * fix exception error
    
    * fix tx fee
    
    * update createNonceTransaction
    
    * fix the prove kbytes length error
    
    * Update VRF.cs
    
    * Update ConsensusService.cs
    
    if the block has no transaction, no need to verify the nonce_tx.
    
    * nonce_tx size check
    
    * Update ConsensusContext.cs
    
    * add nonce to header
    
    * set nonce ulong
    
    * on view change
    
    * maxTransactionsPerBlock
    
    * add nonce to `HeaderFromJson`
    
    * remove reference
    
    * `ulong` to `hexstring` in `ToJson`
    
    * fix hex_str to uint64
    
    * fix format
    
    * remove unused import
    
    * update `recoverymessage`
    
    * Remove VRF
    
    * update random seed
    
    * Generate `nonce` with `Random`. Add `Nonce` field to `Preparerequest`
    
    * add `nonce` to `recorymessage`
    
    * update project `dbft` structure
    
    * remove unused import
    
    * Reset Nonce
    
    * Update DBFT format.
    Update RPC UT cases
    
    * format
    
    * automatically format code while build in release mode.
    
    * update auto-format
    
    * Add auto-format to all Module projects
    
    * update to 3.0.0-CI01285
    
    * Add `Prebuild.targets` to config the auto-format
    
    * auto-format only in Debug mode
    
    * auto format in debug mode
    
    * remove auto-format
    
    * remove unused code in .csproj files
    
    * format
    
    * Reduce changes
    
    * Remove enter
    
    * Update DBFTPlugin.csproj
    
    * format
    
    * format
    
    * Optimize GetNonce
    
    Co-authored-by: Shargon <shargon@gmail.com>
    Co-authored-by: Qiao Jin <jinqiao0423@gmail.com>
    Co-authored-by: nicolegys <825605594@qq.com>
    Co-authored-by: Qiao Jin <43407364+Qiao-Jin@users.noreply.github.com>
    Co-authored-by: Owen Zhang <38493437+superboyiii@users.noreply.github.com>
    Co-authored-by: Erik Zhang <erik@neo.org>

--- src/DBFTPlugin/Messages/PrepareRequest.cs
+++ src/DBFTPlugin/Messages/PrepareRequest.cs
@@ -10,12 +10,14 @@ namespace Neo.Consensus
         public uint Version;
         public UInt256 PrevHash;
         public ulong Timestamp;
+        public ulong Nonce;
         public UInt256[] TransactionHashes;
 
         public override int Size => base.Size
             + sizeof(uint)                      //Version
             + UInt256.Length                    //PrevHash
             + sizeof(ulong)                     //Timestamp
+            + sizeof(ulong)                     // Nonce
             + TransactionHashes.GetVarSize();   //TransactionHashes
 
         public PrepareRequest() : base(ConsensusMessageType.PrepareRequest) { }
@@ -26,6 +28,7 @@ namespace Neo.Consensus
             Version = reader.ReadUInt32();
             PrevHash = reader.ReadSerializable<UInt256>();
             Timestamp = reader.ReadUInt64();
+            Nonce = reader.ReadUInt64();
             TransactionHashes = reader.ReadSerializableArray<UInt256>(ushort.MaxValue);
             if (TransactionHashes.Distinct().Count() != TransactionHashes.Length)
                 throw new FormatException();
@@ -43,6 +46,7 @@ namespace Neo.Consensus
             writer.Write(Version);
             writer.Write(PrevHash);
             writer.Write(Timestamp);
+            writer.Write(Nonce);
             writer.Write(TransactionHashes);
         }
     }
