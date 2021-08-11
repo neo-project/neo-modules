3fef1fd32df72b91ea3c9b65ee59fa68608649c5
Jinghui Liao <jinghui@wayne.edu>
2021-08-11 09:52:41 +0800

    dispose store

--- src/LevelDBStore/Plugins/Storage/LevelDBStore.cs
+++ src/LevelDBStore/Plugins/Storage/LevelDBStore.cs
@@ -1,6 +1,7 @@
 using Neo.IO.Data.LevelDB;
 using Neo.Persistence;
 using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace Neo.Plugins.Storage
@@ -9,11 +10,25 @@ namespace Neo.Plugins.Storage
     {
         public override string Description => "Uses LevelDB to store the blockchain data";
 
+        private List<WeakReference> dbs = new();
+
         public IStore GetStore(string path)
         {
             if (Environment.CommandLine.Split(' ').Any(p => p == "/repair" || p == "--repair"))
                 DB.Repair(path, Options.Default);
-            return new Store(path);
+            IStore store = new Store(path);
+            dbs.Add(new WeakReference(store));
+            return store;
+        }
+        public override void Dispose()
+        {
+            base.Dispose();
+            foreach (var a in dbs)
+            {
+                IStore store = a.Target as IStore;
+                if (store is null) continue;
+                store.Dispose();
+            }
         }
     }
 }
