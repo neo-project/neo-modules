e632ad8b3d52f04a40fa142cb5868c58e2c5834a
Shargon <shargon@gmail.com>
2021-08-24 14:57:56 +0200

    Update src/StateService/MPT/MPTTrie.Proof.cs

--- src/StateService/MPT/MPTTrie.Proof.cs
+++ src/StateService/MPT/MPTTrie.Proof.cs
@@ -13,7 +13,7 @@ namespace Neo.Plugins.MPT
         public bool TryGetProof(TKey key, out HashSet<byte[]> proof)
         {
             var path = ToNibbles(key.ToArray());
-            if (path.Length == 0) throw new ArgumentException("could not be empty", nameof(key)); ;
+            if (path.Length == 0) throw new ArgumentException("could not be empty", nameof(key));
             proof = new HashSet<byte[]>(ByteArrayEqualityComparer.Default);
             return GetProof(ref root, path, proof);
         }
