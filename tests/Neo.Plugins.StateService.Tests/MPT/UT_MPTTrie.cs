e5691e720881b73e87a39a81f38da2c45d247251
ZhangTao1596 <zhangtao@ngd.neo.org>
2021-10-18 10:01:59 +0800

    more ut

--- tests/Neo.Plugins.StateService.Tests/MPT/UT_MPTTrie.cs
+++ tests/Neo.Plugins.StateService.Tests/MPT/UT_MPTTrie.cs
@@ -617,7 +617,7 @@ namespace Neo.Plugins.StateService.Tests
             r = mpt.Find("aa".HexToBytes(), "aa60".HexToBytes()).ToList();
             Assert.AreEqual(0, r.Count);
             r = mpt.Find("aa".HexToBytes(), "aa10".HexToBytes()).ToList();
-            Assert.AreEqual(1, r.Count);//without from key
+            Assert.AreEqual(1, r.Count);
         }
 
         [TestMethod]
@@ -629,6 +629,10 @@ namespace Neo.Plugins.StateService.Tests
             mpt.Put("abc3".HexToBytes(), "02".HexToBytes());
             var r = mpt.Find("ab".HexToBytes(), "abd2".HexToBytes()).ToList();
             Assert.AreEqual(0, r.Count);
+            r = mpt.Find("ab".HexToBytes(), "abb2".HexToBytes()).ToList();
+            Assert.AreEqual(2, r.Count);
+            r = mpt.Find("ab".HexToBytes(), "abc2".HexToBytes()).ToList();
+            Assert.AreEqual(1, r.Count);
         }
     }
 }
