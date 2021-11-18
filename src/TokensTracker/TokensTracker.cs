aaa5ce7126a3aa7a0afde0ea2a5becc9431fa80e
Erik Zhang <erik@neo.org>
2021-11-18 19:59:26 +0800

    Add _enabledTrackers

--- src/TokensTracker/TokensTracker.cs
+++ src/TokensTracker/TokensTracker.cs
@@ -1,35 +1,37 @@
+using Microsoft.Extensions.Configuration;
 using Neo.IO;
-using Neo.IO.Json;
 using Neo.Ledger;
 using Neo.Network.P2P.Payloads;
 using Neo.Persistence;
-using Neo.Plugins.Storage;
-using Neo.SmartContract;
-using Neo.SmartContract.Native;
-using Neo.VM;
-using Neo.VM.Types;
-using Neo.Wallets;
 using System;
-using System.Buffers.Binary;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
-using System.Numerics;
 using static System.IO.Path;
 
 namespace Neo.Plugins
 {
     public class TokensTracker : Plugin, IPersistencePlugin
     {
+        private string _dbPath;
         private bool _shouldTrackHistory;
         private uint _maxResults;
         private uint _network;
-        private string _dbPath;
+        private string[] _enabledTrackers;
         private IStore _db;
         private NeoSystem neoSystem;
         private readonly List<TrackerBase> trackers = new();
 
-        public override string Description => "Enquiries NEP-11 balances and transaction history of accounts through RPC";
+        public override string Description => "Enquiries balances and transaction history of accounts through RPC";
+
+        protected override void Configure()
+        {
+            IConfigurationSection config = GetConfiguration();
+            _dbPath = config.GetValue("DBPath", "TokensBalanceData");
+            _shouldTrackHistory = config.GetValue("TrackHistory", true);
+            _maxResults = config.GetValue("MaxResults", 1000u);
+            _network = config.GetValue("Network", 860833102u);
+            _enabledTrackers = config.GetSection("EnabledTrackers").GetChildren().Select(p => p.Value).ToArray();
+        }
 
         protected override void OnSystemLoaded(NeoSystem system)
         {
@@ -37,18 +39,12 @@ namespace Neo.Plugins
             neoSystem = system;
             string path = string.Format(_dbPath, neoSystem.Settings.Network.ToString("X8"));
             _db = neoSystem.LoadStore(GetFullPath(path));
-            trackers.Add(new Nep11Tracker(_db, _maxResults, _shouldTrackHistory, neoSystem));
-            trackers.Add(new Nep17Tracker(_db, _maxResults, _shouldTrackHistory, neoSystem));
+            if (_enabledTrackers.Contains("NEP-11"))
+                trackers.Add(new Nep11Tracker(_db, _maxResults, _shouldTrackHistory, neoSystem));
+            if (_enabledTrackers.Contains("NEP-17"))
+                trackers.Add(new Nep17Tracker(_db, _maxResults, _shouldTrackHistory, neoSystem));
             foreach (TrackerBase tracker in trackers)
                 RpcServerPlugin.RegisterMethods(tracker, _network);
-        }
-
-        protected override void Configure()
-        {
-            _dbPath = GetConfiguration().GetSection("DBPath").Value ?? "TokensBalanceData";
-            _shouldTrackHistory = (GetConfiguration().GetSection("TrackHistory").Value ?? true.ToString()) != false.ToString();
-            _maxResults = uint.Parse(GetConfiguration().GetSection("MaxResults").Value ?? "1000");
-            _network = uint.Parse(GetConfiguration().GetSection("Network").Value ?? "860833102");
         }
 
         private void ResetBatch()
