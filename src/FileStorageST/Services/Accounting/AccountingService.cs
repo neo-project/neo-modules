9dfbacfdb74e3d917dfc3955b9bc5a1ebcf2a44b
ZhangTao1596 <zhangtao@ngd.neo.org>
2022-02-23 11:24:50 +0800

    Services/Accounting: update

--- src/FileStorageST/Services/Accounting/AccountingService.cs
+++ src/FileStorageST/Services/Accounting/AccountingService.cs
@@ -1,4 +1,3 @@
-using System.Numerics;
 using Neo.FileStorage.API.Accounting;
 using Neo.FileStorage.Invoker.Morph;
 using Neo.IO;
@@ -15,26 +14,16 @@ namespace Neo.FileStorage.Storage.Services.Accounting
         {
             var owner = request.Body?.OwnerId;
             OwnerIDCheck(owner);
-            var balance = (BigInteger)MorphInvoker.BalanceOf(request.Body.OwnerId.ToScriptHash().ToArray());
-            byte decimals = (byte)MorphInvoker.BalanceDecimals();
-            if (decimals > Fixed8Precision)
-            {
-                BigInteger divisor = BigInteger.Pow(10, decimals - Fixed8Precision);
-                balance = BigInteger.Divide(balance, divisor);
-            }
-            else if (decimals < Fixed8Precision)
-            {
-                BigInteger divisor = BigInteger.Pow(10, Fixed8Precision - decimals);
-                balance = balance * divisor;
-            }
+            var balance = MorphInvoker.BalanceOf(request.Body.OwnerId.ToScriptHash().ToArray());
+            var decimals = MorphInvoker.BalanceDecimals();
             var resp = new BalanceResponse
             {
                 Body = new BalanceResponse.Types.Body
                 {
                     Balance = new Decimal
                     {
-                        Value = (long)balance,
-                        Precision = Fixed8Precision,
+                        Value = balance,
+                        Precision = decimals,
                     }
                 }
             };
