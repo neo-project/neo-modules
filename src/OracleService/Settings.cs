437cb3792315cda01926ade0fe52538fcc1208eb
Shargon <shargon@gmail.com>
2022-04-19 12:03:12 +0200

    Create new field

--- src/OracleService/Settings.cs
+++ src/OracleService/Settings.cs
@@ -41,6 +41,7 @@ namespace Neo.Plugins
         public uint Network { get; }
         public Uri[] Nodes { get; }
         public TimeSpan MaxTaskTimeout { get; }
+        public TimeSpan MaxOracleTimeout { get; }
         public bool AllowPrivateHost { get; }
         public string[] AllowedContentTypes { get; }
         public HttpsSettings Https { get; }
@@ -54,6 +55,7 @@ namespace Neo.Plugins
             Network = section.GetValue("Network", 5195086u);
             Nodes = section.GetSection("Nodes").GetChildren().Select(p => new Uri(p.Get<string>(), UriKind.Absolute)).ToArray();
             MaxTaskTimeout = TimeSpan.FromMilliseconds(section.GetValue("MaxTaskTimeout", 432000000));
+            MaxOracleTimeout = TimeSpan.FromMilliseconds(section.GetValue("MaxOracleTimeout", 10000));
             AllowPrivateHost = section.GetValue("AllowPrivateHost", false);
             AllowedContentTypes = section.GetSection("AllowedContentTypes").GetChildren().Select(p => p.Get<string>()).ToArray();
             Https = new HttpsSettings(section.GetSection("Https"));
