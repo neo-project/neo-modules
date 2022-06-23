c4a446d2efc268892fbcbb161ef151bd1f1c1b57
ZhangTao <zhangtao@ngd.neo.org>
2022-06-23 18:08:39 +0800

    Fix find states (#734)
    * fix findstates
    
    * fix key results

--- src/StateService/StatePlugin.cs
+++ src/StateService/StatePlugin.cs
@@ -23,6 +23,7 @@ using Neo.SmartContract;
 using Neo.SmartContract.Native;
 using Neo.Wallets;
 using System;
+using System.Buffers.Binary;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -188,11 +189,16 @@ namespace Neo.Plugins.StateService
 
         private string GetProof(Trie trie, int contract_id, byte[] key)
         {
-            StorageKey skey = new StorageKey
+            StorageKey skey = new()
             {
                 Id = contract_id,
                 Key = key,
             };
+            return GetProof(trie, skey);
+        }
+
+        private string GetProof(Trie trie, StorageKey skey)
+        {
             var result = trie.TryGetProof(skey.ToArray(), out var proof);
             if (!result) throw new KeyNotFoundException();
 
@@ -275,6 +281,15 @@ namespace Neo.Plugins.StateService
             return trie.TryGetValue(skey.ToArray(), out var value) ? value.AsSerializable<StorageItem>().GetInteroperable<ContractState>() : null;
         }
 
+        private StorageKey ParseStorageKey(byte[] data)
+        {
+            return new()
+            {
+                Id = BinaryPrimitives.ReadInt32LittleEndian(data),
+                Key = data.AsMemory(sizeof(int)),
+            };
+        }
+
         [RpcMethod]
         public JObject FindStates(JArray _params)
         {
@@ -314,7 +329,7 @@ namespace Neo.Plugins.StateService
                 if (i < count)
                 {
                     JObject j = new();
-                    j["key"] = Convert.ToBase64String(ikey.Span);
+                    j["key"] = Convert.ToBase64String(ParseStorageKey(ikey.ToArray()).Key.Span);
                     j["value"] = Convert.ToBase64String(ivalue.Span);
                     jarr.Add(j);
                 }
