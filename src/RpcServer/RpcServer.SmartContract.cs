12ded50e9e96147aa4c5369289a5267215c25de1
Jinghui Liao <jinghui@wayne.edu>
2022-07-12 21:40:19 -0400

    revert name change

--- src/RpcServer/RpcServer.SmartContract.cs
+++ src/RpcServer/RpcServer.SmartContract.cs
@@ -199,7 +199,7 @@ namespace Neo.Plugins
         [RpcMethod]
         protected virtual JObject InvokeFunction(JArray _params)
         {
-            UInt160 scriptHash = UInt160.Parse(_params[0].AsString());
+            UInt160 script_hash = UInt160.Parse(_params[0].AsString());
             string operation = _params[1].AsString();
             ContractParameter[] args = _params.Count >= 3 ? ((JArray)_params[2]).Select(p => ContractParameter.FromJson(p)).ToArray() : System.Array.Empty<ContractParameter>();
             Signer[] signers = _params.Count >= 4 ? SignersFromJson((JArray)_params[3], system.Settings) : null;
@@ -209,7 +209,7 @@ namespace Neo.Plugins
             byte[] script;
             using (ScriptBuilder sb = new())
             {
-                script = sb.EmitDynamicCall(scriptHash, operation, args).ToArray();
+                script = sb.EmitDynamicCall(script_hash, operation, args).ToArray();
             }
             return GetInvokeResult(script, signers, witnesses, useDiagnostic);
         }
@@ -262,20 +262,20 @@ namespace Neo.Plugins
         {
             string address = _params[0].AsString();
             JObject json = new();
-            UInt160 scriptHash;
+            UInt160 script_hash;
             try
             {
-                scriptHash = AddressToScriptHash(address, system.Settings.AddressVersion);
+                script_hash = AddressToScriptHash(address, system.Settings.AddressVersion);
             }
             catch
             {
-                scriptHash = null;
+                script_hash = null;
             }
-            if (scriptHash == null)
+            if (script_hash == null)
                 throw new RpcException(-100, "Invalid address");
             var snapshot = system.StoreView;
-            json["unclaimed"] = NativeContract.NEO.UnclaimedGas(snapshot, scriptHash, NativeContract.Ledger.CurrentIndex(snapshot) + 1).ToString();
-            json["address"] = scriptHash.ToAddress(system.Settings.AddressVersion);
+            json["unclaimed"] = NativeContract.NEO.UnclaimedGas(snapshot, script_hash, NativeContract.Ledger.CurrentIndex(snapshot) + 1).ToString();
+            json["address"] = script_hash.ToAddress(system.Settings.AddressVersion);
             return json;
         }
 
